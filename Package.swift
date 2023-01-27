30bfe2a5ae459efdeb9945a9e06d8ace2b301590
tatsushi goda <t.goda@xtone.co.jp>
2023-01-27 13:53:07 +0900

    fix package

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "SwiftAutoScreenShot",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary", "SomeRemoteBinaryPackage", "SomeLocalBinaryPackage"])
+            name: "SwiftAutoScreenShot",
+            targets: ["SwiftAutoScreenShot"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,20 +20,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary"
-        ),
         .binaryTarget(
-            name: "SomeRemoteBinaryPackage",
-            url: "https://url/to/some/remote/xcframework.zip",
-            checksum: "The checksum of the ZIP archive that contains the XCFramework."
-        ),
-        .binaryTarget(
-            name: "SomeLocalBinaryPackage",
-            path: "path/to/some.xcframework"
+            name: "SwiftAutoScreenShot",
+            path: "SwiftAutoScreenShot.xcframework"
         )
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
     ]
 )
