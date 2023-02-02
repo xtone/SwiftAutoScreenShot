a269e16fad2541dc2092e51c7df3223b0fb493d3
tatsushi goda <t.goda@xtone.co.jp>
2023-02-02 16:05:34 +0900

    deubg function add

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "SwiftAutoScreenShot",
-            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0201_ver0.10001/SwiftAutoScreenShot.xcframework.zip",
-            checksum: "a6d9e2a52bb1cc1628debb4a1f2b157ee3267cb352edc7212f0dec4ae9f19f40"
+            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0202_ver0.10002/SwiftAutoScreenShot.xcframework.zip",
+            checksum: "0612828ea27568ea5c5caec5cb990beee091ba73785e2b2b97db257d41b1526c"
         )
     ]
 )
