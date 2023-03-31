26284ebe6d5943dd8c83b1ed20671ff531ea12e6
tatsushi goda <t.goda@xtone.co.jp>
2023-03-31 12:13:36 +0900

    topオフセット計算修正

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "SwiftAutoScreenShot",
-            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0331_ver0.10036/SwiftAutoScreenShot.xcframework.zip",
-            checksum: "87e74a927a4776d7fdceedee458330e143b69dde96c9a0d5684c57480ee486f2"
+            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0332_ver0.10036/SwiftAutoScreenShot.xcframework.zip",
+            checksum: "cfb85a523ecb09209dffaea82d8f31538764c5b91d93c279635a60ce543c53c9"
         )
     ]
 )
