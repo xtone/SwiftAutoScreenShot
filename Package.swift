87eab9eb8a2bbb60c07de568af36100152b0308d
tatsushi goda <t.goda@xtone.co.jp>
2023-03-31 17:11:19 +0900

    スクロール調整

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "SwiftAutoScreenShot",
-            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0332_ver0.10037/SwiftAutoScreenShot.xcframework.zip",
-            checksum: "80b741df9d42b3febf0cd9ed4321126fb76bd810e847a2254d8d39cd141e0157"
+            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0332_ver0.10038/SwiftAutoScreenShot.xcframework.zip",
+            checksum: "f6394ca3c8e6ba618f1a1ad5afd0d3cd373274d15381a6c9c9cdbf2feaedf4e3"
         )
     ]
 )
