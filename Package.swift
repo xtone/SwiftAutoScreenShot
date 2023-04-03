b91cc2f356ed2b5e665ccac6ba4f0ecc82895246
tatsushi goda <t.goda@xtone.co.jp>
2023-04-03 11:13:40 +0900

    スクロールが必要な画面で2枚しか画像がない場合の不具合を修正

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "SwiftAutoScreenShot",
-            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0332_ver0.10038/SwiftAutoScreenShot.xcframework.zip",
-            checksum: "f6394ca3c8e6ba618f1a1ad5afd0d3cd373274d15381a6c9c9cdbf2feaedf4e3"
+            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0403_ver0.10039/SwiftAutoScreenShot.xcframework.zip",
+            checksum: "ef4c16c6f0c6b472d170a878a6d91d61087b14fc9067cd422d62b8e5d980e111"
         )
     ]
 )
