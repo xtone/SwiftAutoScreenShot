e89fdbad2695fb2f51e82ee8407f13c89fb67648
tatsushi goda <t.goda@xtone.co.jp>
2023-05-08 16:07:09 +0900

    画像のAffine変換処理を一部修正

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "SwiftAutoScreenShot",
-            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0502_ver0.10066/SwiftAutoScreenShot.xcframework.zip",
-            checksum: "f9007f25f938fc40182b2f4321639dd545d61ffbca739087ff2b19c63ac76c59"
+            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0508_ver0.10067/SwiftAutoScreenShot.xcframework.zip",
+            checksum: "5b1f3868f544bdefdccc1c370a88dfd2751ba6297a2aad88b1123ebed965a9b6"
         )
     ]
 )
