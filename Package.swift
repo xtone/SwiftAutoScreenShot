dce1a4d3687a52f94331b1e6f9b55723965f9edb
tatsushi goda <t.goda@xtone.co.jp>
2023-06-26 16:23:28 +0900

    fixed double size image output

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "SwiftAutoScreenShot",
-            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0626_ver0.10089/SwiftAutoScreenShot.xcframework.zip",
-            checksum: "140268a20abcaca6f8064c9c6764d28d43f68793cd73f8b905810ec9ebb2257c"
+            url: "https://github.com/xtone/SwiftAutoScreenShot/releases/download/2023_0626_ver0.10090/SwiftAutoScreenShot.xcframework.zip",
+            checksum: "f5655f2debf5e26f209a986331e40560255aba6cf5b8e4be451a66a090960957"
         )
     ]
 )
